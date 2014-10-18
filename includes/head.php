ea0a701984684cc2fff7e02680434fb6e991af03
saftaclaudia <saftaclaudia27@yahoo.com>
2014-10-19 00:11:57 +0300

    homepage
    php and css

---
+++ includes/head.php
@@ -0,0 +1,9 @@
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.14.1/build/cssreset/cssreset-min.css">
+	<link type="text/css" rel="stylesheet" href="clearfix.css"/>
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
+	<link type="text/css" rel="stylesheet" href="homepagestylesheet.css"/>
+	<link type= "text/css" rel="stylesheet" href="servicesstylesheet.css"/>
+	<link type= "text/css" rel="stylesheet" href="portfoliostylesheet.css"/>
+	<link type= "text/css" rel="stylesheet" href="aboutstyelsheet.css"/>
+	<link type= "text/css" rel="stylesheet" href="contactstyelsheet.css"/>
+	<title><?php echo $title ?></title>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php 
+	$title="homepage";
+	require('includes/head.php');
+?>
+</head>
+
+<body>
+	<div class="header">
+		<div class= "wrapper">
+			<h1>IDDO</h1>
+			<ul class="head_manue">
+				<li><a href= "index.php">Home</a></li>
+				<li><a href="services.php">Services</a></li>
+				<li><a href="portfolio.php">Portfolio</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+			<input type="button" value= "Sign Up" id="sign_up" class="blue">
+		</div>
+	</div>
+	<div class="central_content">
+		<div class="wrapper">
+			<div data-media="file1.mp3" class="play"></div>
+			<div class="text">
+				<h2>Let me show you a few things.</h2>
+				<a href ="#" class="blue">Click to play video</a>
+			</div>
+		</div>	
+	</div>
+	<div class="footer">
+		<div class= "wrapper">
+			<ul class= "footer_content">
+				<li>IDDO</li>
+				<li>@ 2014 IDDO. All rights reserved.</li>
+				<li>Designed by DZOAN.</li>
+			</ul>
+		</div>
+	</div>
+
+
+</body>
+</html>
