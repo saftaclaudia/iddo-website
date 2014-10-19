6704e0c3c7df76fd55f4b9dbe5350de8516296aa
saftaclaudia <saftaclaudia27@yahoo.com>
2014-10-19 23:05:50 +0300

    services page
    php and css for service page

--- services.php
+++ services.php
@@ -11,10 +11,59 @@
 	<?php 
 		require('includes/header.php');
 	?>
+	<div class = "content">
+		<div class="wrapper">
+			<div class="float width20">
+				<p>OUR SERVICES.</p>
+			</div>
+			<div class="float width37">
+				<div class="feature">
+					<img src="images/img1.png"/>
+					<ul>
+						<li>FEATURE 1 </li>
+						<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</li>
+					</ul>
+				</div>
+				<div class="feature">
+					<img src="images/img2.png"/>
+					<ul>
+						<li>FEATURE 2</li>
+						<li>Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</li>
+					</ul>
+				</div>
+			</div>
+			<div class="right width37">
+				<div class="feature">
+					<img src="images/img3.png"/>
+					<ul>
+						<li>FEATURE 3 </li>
+						<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</li>
+					</ul>
+				</div>
+				<div class="feature">
+					<img src="images/img4.png"/>
+					<ul>
+						<li>FEATURE 4</li>
+						<li>Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="bottom_menu">
+		<div class="wrapper">
+			<ul>
+				<li><a href="#"><img src="images/ico1.png"/></a></li>
+				<li><a href="#"><img src="images/ico2.png"/></a></li>
+				<li><a href="#"><img src="images/ico3.png"/></a></li>
+				<li><a href="#"><img src="images/ico4.png"/></a></li>
+				<li><a href="#"><img src="images/ico5.png"/></a></li>
+				<li><a href="#"><img src="images/ico6.png"/></a></li>
+		</div>
+	</div>
+
 	<?php 
 	require('includes/footer.php');
 	?>
-
-
 </body>
 </html>
